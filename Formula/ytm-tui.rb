3ddf57b10cd48ea85ce223ea88349f2bc825c391
Ochichan <ohstar94@gmail.com>
2026-06-28 00:32:49 +0900

    Update ytm-tui to 1.2.0

--- Formula/ytm-tui.rb
+++ Formula/ytm-tui.rb
@@ -1,8 +1,8 @@
 class YtmTui < Formula
   desc "Fast, low-RAM YouTube Music player for your terminal"
   homepage "https://github.com/Ochichan/ytm-tui"
-  url "https://github.com/Ochichan/ytm-tui/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "1ccd03f4ef6e5112bdae3297f90bde751de41dd65862e352e75c447382757f0a"
+  url "https://github.com/Ochichan/ytm-tui/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "b2221835cad9c1136d0ab49adb798048d80702996726bde85dd163e89d5a5be0"
   license "MIT"
   head "https://github.com/Ochichan/ytm-tui.git", branch: "main"
 
